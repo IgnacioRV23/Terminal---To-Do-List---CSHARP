f0b60f487bb864f68c4f3ef6abac242aa6d212d2
Ignacio Rodriguez <nacho.rv2304@gmail.com>
2025-02-19 16:43:16 -0600

    initial commit

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using ConsoleTableExt;
+
+namespace app
+{ 
+    class Program
+    {
+        static void Main()
+        {
+            Console.Title = "Console 🔥";
+
+            textArt.Welcome();
+
+
+            var tableData = new List<List<object>>
+{
+                new List<object>{ "Sakura Yamamoto", "Support Engineer", "London", 46},
+                new List<object>{ "Serge Baldwin", "Data Coordinator", "San Francisco", 28, "something else" },
+                new List<object>{ "Shad Decker", "Regional Director", "Edinburgh"},
+};
+
+            ConsoleTableBuilder
+                .From(tableData)
+                .WithTitle("CONTACTS ", ConsoleColor.White, ConsoleColor.DarkGray)
+                .WithColumn("Id", "First Name", "Sur Name").ExportAndWriteLine();
+
+
+            Console.ReadKey();
+        }
+
+        public static void SuccessMsg(string msg)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(msg);
+            Console.ResetColor();
+        }
+    }
+}
